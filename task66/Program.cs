156134d2eab339171c3615b4d2cce4db8b35cd34
modzhin <modzhds@gmail.com>
2023-02-04 22:28:51 +0300

    Task66

---
+++ task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int Input(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SearchSumma(int min, int max)
+{
+    if (min > max) return 0;
+    else return min + SearchSumma(min + 1, max);
+}
+
+int firstNum = Input("Введите минимальное число: ");
+int secondNum = Input("Введите максимальное число: ");
+if (firstNum >= secondNum) Console.WriteLine("Минимальное число должно быть меньше максимального.");
+else
+{
+    Console.WriteLine($"Сумма элементов от {firstNum} до {secondNum} = {SearchSumma(firstNum, secondNum)}");
+}
